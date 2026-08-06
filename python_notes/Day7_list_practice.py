274ad43a29bc0da7861a9aa038a9ed4754728bd9
KrishnaVeni782 <chinnidayaka@gmail.com>
2026-08-06 16:07:33 +0530

    Add files via upload

---
+++ python_notes/Day7_list_practice.py
@@ -0,0 +1,61 @@
+
+list=[10,20,"Krishna",13.2,10,"Veni"]
+print(list)
+print(type(list))
+
+#List operations in Python
+#concatination
+k=["krishna",10,20]
+l=["Veni",77,12.7]
+print(k+l)
+#Repetation
+print(k*3)
+#Access Elements by using indexing
+print(k[0])
+print(l[0])
+#Slicing 
+print(k[::])
+print(k[::2])
+#membership operator(in,not in)
+print("Veni" in l)
+print("Krishnaveni" in k)
+
+#List Build in functions in python
+#len
+print(len(k))
+print(len(k[0])) 
+
+n=[10,30,99,1000]
+print(len(n))
+print(min(n))
+print(max(n))
+print(sum(n))
+print(sorted(n))
+print(any(n))
+print(all(n))
+
+#List Build in methods in python
+
+k=[10,30,100]
+k.append(11)
+print(k)
+k.insert(2,110)
+print(k)
+k.extend([33,99])
+print(k)
+
+k.remove(11)
+print(k)
+print(k.count(10))
+k.sort()
+print(k)
+k.reverse()
+print(k)
+
+t=k
+print(t)
+
+
+
+
+
